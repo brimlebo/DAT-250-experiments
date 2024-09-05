f18e34a5aa5889f6801ead87b36d8baa60cc9aba
Brian.Jensvoll-Jorgensen <brian.jensvoll-jorgensen@student.uib.no>
2024-09-05 12:02:50 +0200

    Initial commit

---
+++ src/main/java/ex/exp1/Exp1Application.java
@@ -0,0 +1,21 @@
+package ex.exp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Exp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp1Application.class, args);
+	}
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
